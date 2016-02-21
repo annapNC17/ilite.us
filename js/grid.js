0113305269e22a37666af847209721d42545f4fe
annapNC17 <n.c.annap@gmail.com>
2016-02-20 23:55:38 -0500

    Update

---
+++ js/grid.js
@@ -0,0 +1,13 @@
+﻿$(window).ready(updateHeight);
+$(window).resize(updateHeight);
+
+function updateHeight()
+{
+    var div = $('.grid-item');
+    var width = div.width();
+    
+    div.css('height', width);
+    $('.footer').css('height', width);
+    $('.skewed').css('margin-top', -(width/1.5));
+}
+
